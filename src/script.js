4adb078919f626a4b2acc7db0cd6dabfe87bc2f0
Muhromin <138580927+romincuy@users.noreply.github.com>
2023-10-10 18:13:43 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 // typing text hero
 const typed = new Typed(".typing-text", {
-  strings: ["Student", "Front-End Developer"],
+  strings: ["Front-End Developer"],
   loop: true,
   typeSpeed: 55,
   backSpeed: 25,
